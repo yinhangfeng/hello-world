a96e8f23aa1b5745fecb97c533e218915521d791
yinhf <darkwander.y@gmail.com>
2017-08-14 11:15:46 +0800

    xxx

--- web/rollup-webpack-example/lib/test.js
+++ web/rollup-webpack-example/lib/test.js
@@ -1,8 +1,34 @@
-import { name } from '../package.json';
+// /test.js
+
+import {
+  name
+} from '../package.json';
 
 global.__DEV__ = true;
 
-export default function test(a) {
-  console.log('test:', a, name);
+function test_1(...a) {
+  console.log(a);
+  const x = Array.from(a);
+  console.log(x);
+
+  for (let a1 of a) {
+    console.log(a1);
+  }
+}
+
+function test_2(a = 1, b = {}) {
+  console.log(a, b);
+}
+
+const xxx = function () {
+
+}
+
+export default function test(a, ...b) {
+  console.log('test:', a, name, b);
+  test_1(1, 2, 3);
+  test_2(2, {
+    x: 2
+  });
   return a;
 }
